d743052a722ae0dbe227f7f386225e20c5347ac0
Kristina Rudyte <rkristina32@yahoo.com>
2022-03-30 14:31:07 +0300

    practise 1

---
+++ practice/01-exercise.js
@@ -0,0 +1,32 @@
+const a = 90;
+console.log(a);
+
+const b = 20;
+console.log(b);
+
+const c = 10;
+console.log(c);
+
+const vardas = 'saule';
+console.log(vardas);
+
+const vardas2 = 'rita';
+console.log(vardas2);
+
+const vardas3 = 'marco';
+console.log(vardas3);
+
+const all = [12, 2, 23, 3, 45];
+console.log(all);
+
+const todo = ['rita', 'lija', 'marco', 'tijo']
+console.log(todo);
+
+console.log(a + b);
+
+console.log(a - b + c);
+console.log(vardas, vardas2, vardas3);
+// palyginimai-------------------------
+
+
+
