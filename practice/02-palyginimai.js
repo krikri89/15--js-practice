9b9c0965b3593761dd5dc8e5b0f1d382528510b7
Kristina Rudyte <rkristina32@yahoo.com>
2022-03-30 15:04:07 +0300

    if

---
+++ practice/02-palyginimai.js
@@ -0,0 +1,48 @@
+const a = 2;
+const b = 5;
+const c = 8;
+const d = 10;
+//pirmas klausimas
+if (a > b) {
+    console.log("Pomidoras");
+} else {
+    console.log("bandykite dar karta");}
+// antras klausimas
+if (a < b)
+{console.log("Pomidoras");}
+else
+{console.log('Bandykite dar karta');};
+//3as klausimas
+if (a >= b)
+{console.log("Pomidoras");}
+else
+{console.log('Bandykite dar karta');};
+/* 4klausimas---*/
+if (a <= b)
+{console.log("Pomidoras");}
+else
+{console.log('Bandykite dar karta');};
+// isvesti teksto kintamuju ilgius
+const text = ['ama', 'ada', 'ana', 'ala', 'aba']
+const textIlgis = text.length;
+
+const text2 = ['baba', 'bebe', 'bibi', 'bobo']
+const text2Ilgis = text2.length;
+
+if (text < text2)
+{console.log('Agurkas');}
+else {
+    console.log('ieskok kitur');
+}
+// 5as klausimas b
+if (text > text2){
+    console.log('good');
+} else {
+    console.log('neteisingai');
+}
+// 5as klausimas c
+if (text = text2)
+{console.log('well done');}
+else {console.log('not really');};
+
+
